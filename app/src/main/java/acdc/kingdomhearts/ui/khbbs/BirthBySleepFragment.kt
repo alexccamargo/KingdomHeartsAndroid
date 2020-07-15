c18690ece9e8ac7984b2dfd8939f31fc0d3f7e6f
alex camargo <alexccamargo@gmail.com>
2020-07-14 22:10:07 -0400

    Added swipeable tabs

--- app/src/main/java/acdc/kingdomhearts/ui/khbbs/BirthBySleepFragment.kt
+++ app/src/main/java/acdc/kingdomhearts/ui/khbbs/BirthBySleepFragment.kt
@@ -5,44 +5,110 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
 import com.google.gson.Gson
 import java.nio.charset.Charset
+import com.google.android.material.tabs.TabLayoutMediator
+
 
 class BirthBySleepFragment : Fragment() {
 
     private lateinit var birthBySleepViewModel: BirthBySleepViewModel
+    private lateinit var viewPager: ViewPager2
+    private lateinit var birthBySleepCollectionAdapter: BirthBySleepCollectionAdapter
+    private lateinit var effects: EffectsModel
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-        birthBySleepViewModel =
-                ViewModelProviders.of(this).get(BirthBySleepViewModel::class.java)
+        birthBySleepViewModel = ViewModelProvider(this).get(BirthBySleepViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_khbbs, container, false)
 
-        val effects = Gson().fromJson<EffectsModel>(readFile(R.raw.khbbs_effects), EffectsModel::class.java)
-
-        val textView: TextView = root.findViewById(R.id.textView2)
-        textView.text = effects.data.first().description
-
+        effects =
+            Gson().fromJson<EffectsModel>(readFile(R.raw.khbbs_effects), EffectsModel::class.java)
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        birthBySleepCollectionAdapter = BirthBySleepCollectionAdapter(this)
+        viewPager = view.findViewById(R.id.pager)
+        viewPager.adapter = birthBySleepCollectionAdapter
+
+        val tabLayout: TabLayout = view.findViewById(R.id.tab_layout)
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            when (position) {
+                0 -> {
+                    tab.text = context?.getString(R.string.khbbs_tabs_all)
+                }
+                1 -> {
+                    tab.text = context?.getString(R.string.khbbs_tabs_effect)
+                }
+                2 -> {
+                    tab.text = context?.getString(R.string.khbbs_tabs_command)
+                }
+            }
+
+        }.attach()
     }
 
     private fun readFile(rID: Int): String {
         val iS = resources.openRawResource(rID);
-
         val size: Int = iS.available()
-
         val buffer = ByteArray(size)
-
         iS.read(buffer)
-
         iS.close()
-
         return String(buffer, Charset.defaultCharset())
+    }
+}
+
+class BirthBySleepCollectionAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
+
+    override fun getItemCount(): Int = 3
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> TabAllPageFragment()
+            1 -> TabPerCommandPageFragment()
+            else -> TabPerEffectPageFragment()
+        }
+    }
+}
+
+class TabAllPageFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_khbbs_tab_all, container, false)
+    }
+}
+
+class TabPerCommandPageFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_khbbs_tab_per_command, container, false)
+    }
+}
+
+class TabPerEffectPageFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_khbbs_tab_per_effect, container, false)
     }
 }
